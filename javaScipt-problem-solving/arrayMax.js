936cb95db3665f8ea8eb50e23bc0721506a10dfa
Md. Abbas Ali <alabbashossain@gmail.com>
2021-01-20 21:10:13 +0600

    array max function

--- javaScipt-problem-solving/arrayMax.js
+++ javaScipt-problem-solving/arrayMax.js
@@ -1,9 +1,22 @@
-var marks = [45, 81, 63, 98, 56, 35, 23];
-var max = marks[0];
-for( var i=0; i <marks.length; i++){
-    var element = marks[i];
-    if(element > max){
-        max = element;
+// let marks = [45, 81, 63, 98, 56, 35, 23];
+// let max = marks[0];
+// for (let i = 0; i < marks.length; i++) {
+//     let element = marks[i];
+//     if (element > max) {
+//         max = element;
+//     }
+// }
+// console.log("Highest value is:", max);
+
+function arrayMax(list) {
+    let max = list[0];
+    for (let i = 0; i < list.length; i++) {
+        let total = list[i];
+        if (total > max) {
+            max = total;
+        }
     }
+    return max;
 }
-console.log("Highest value is:", max);
+let result = arrayMax([12, 3, 43, 54, 32, 42]);
+console.log(result);
